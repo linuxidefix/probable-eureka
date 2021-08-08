55f01055f61aaa136b2feb96e4146c5c84791e27
linuxidefix <karim.pub@gmail.com>
2021-08-08 22:25:10 +0200

    add version

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ import { context } from '@actions/github';
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = getInput('who-to-greet');
+  const version = getInput('version');
+  const newVersion = version + 1;
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   setOutput("time", time);
+  setOutput("newVersion", newVersion);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
